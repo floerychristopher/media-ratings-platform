b85533e2a82739a591c1aa8ec3c82ca4a9726a7c
Christopher Floery <realchristopherfloery@gmail.com>
2026-03-10 02:36:59 +0100

    Add LeaderboardController.java

---
+++ src/main/java/mrp/controller/LeaderboardController.java
@@ -0,0 +1,28 @@
+package mrp.controller;
+
+import mrp.model.User;
+import mrp.server.HttpRequest;
+import mrp.server.HttpResponse;
+import mrp.service.UserService;
+import mrp.util.JsonUtil;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class LeaderboardController {
+    private final UserService userService;
+
+    public LeaderboardController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // GET /api/leaderboard
+    public HttpResponse getLeaderboard(HttpRequest req) {
+        try {
+            List<User> leaderboard = userService.getLeaderboard();
+            return HttpResponse.ok(JsonUtil.toJson(leaderboard));
+        } catch (SQLException e) {
+            return HttpResponse.internalError("Database error: " + e.getMessage());
+        }
+    }
+}
